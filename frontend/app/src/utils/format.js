c1a47ad5f2d3671bc757dbe06427ce74de3ebc53
UnderLoki <ajavondavidayi@gmail.com>
2026-09-07 19:01:08 +0000

    chore(merge): stabiliser l integration de main dans stage
    - conserve les versions recentes de l accueil et des opportunites participant
    
    - retire une page admin et un service commanditaire non references
    
    - supprime un utilitaire inutilise et un lockfile racine vide

--- frontend/app/src/utils/format.js
+++
@@ -1,8 +0,0 @@
-export function formatMontant(value, options = {}) {
-  const { decimals = 0 } = options
-  const amount = Number(value || 0)
-  return amount.toLocaleString('fr-FR', {
-    minimumFractionDigits: decimals,
-    maximumFractionDigits: decimals,
-  }).replace(/[\u202f\u00a0]/g, ' ')
-}
